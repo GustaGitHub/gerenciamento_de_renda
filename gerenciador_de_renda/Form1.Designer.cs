dc28744bbe5af3860bb9e9145a3fe2e5f143f5e5
GustaGitHub <gustavo.ribeiro.duarte2003@gmail.com>
2022-06-23 22:50:14 -0300

    Adicionar arquivos de projeto.

---
+++ gerenciador_de_renda/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace gerenciador_de_renda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.valorRenda = new System.Windows.Forms.TextBox();
+            this.botaoForm = new System.Windows.Forms.Button();
+            this.resultados = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(8, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(299, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Informe sua Renda Mensal (Ex: 2500,75)";
+            // 
+            // valorRenda
+            // 
+            this.valorRenda.Font = new System.Drawing.Font("Arial", 12F);
+            this.valorRenda.Location = new System.Drawing.Point(12, 61);
+            this.valorRenda.Multiline = true;
+            this.valorRenda.Name = "valorRenda";
+            this.valorRenda.Size = new System.Drawing.Size(295, 31);
+            this.valorRenda.TabIndex = 1;
+            // 
+            // botaoForm
+            // 
+            this.botaoForm.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.botaoForm.Location = new System.Drawing.Point(323, 61);
+            this.botaoForm.Name = "botaoForm";
+            this.botaoForm.Size = new System.Drawing.Size(148, 31);
+            this.botaoForm.TabIndex = 2;
+            this.botaoForm.Text = "Calcular";
+            this.botaoForm.UseVisualStyleBackColor = true;
+            this.botaoForm.Click += new System.EventHandler(this.OrganizarRenda);
+            // 
+            // resultados
+            // 
+            this.resultados.Font = new System.Drawing.Font("Arial", 12F);
+            this.resultados.FormattingEnabled = true;
+            this.resultados.ItemHeight = 18;
+            this.resultados.Location = new System.Drawing.Point(12, 142);
+            this.resultados.Name = "resultados";
+            this.resultados.Size = new System.Drawing.Size(459, 94);
+            this.resultados.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 260);
+            this.Controls.Add(this.resultados);
+            this.Controls.Add(this.botaoForm);
+            this.Controls.Add(this.valorRenda);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Gerenciamento de Renda";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox valorRenda;
+        private System.Windows.Forms.Button botaoForm;
+        private System.Windows.Forms.ListBox resultados;
+    }
+}
+
---
+++ gerenciador_de_renda/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace gerenciador_de_renda
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void EstratificarRenda(double renda)
+        {
+            Dictionary<string, double> estratos = new Dictionary<string, double>
+            {
+                { "estrato50", (renda * 50) / 100 }, // 50% Para Gastos Essenciais
+                { "estrato35", (renda * 35) / 100 }, // 35% Para Estilo de vida
+                { "estrato15", (renda * 15) / 100 } // 15% Para Investimentos
+            };
+
+            double estrato50 = estratos["estrato50"];
+            double estrato35 = estratos["estrato35"];
+            double estrato15 = estratos["estrato15"];
+
+            //Método para imprimir os resultados no formulário
+            ExibirResultados(renda, estrato50, estrato35, estrato15);
+        }
+
+        private void ExibirResultados(double renda, double estrato50, double estrato35, double estrato15)
+        {
+            resultados.Items.Clear();
+            resultados.Items.Add($"- Para {renda.ToString("C")} Faça o seguinte:");
+            resultados.Items.Add(" ");
+            resultados.Items.Add($"- Reserve {estrato50.ToString("C")} (50%), para GASTOS ESSENCIAIS.");
+            resultados.Items.Add($"- Reserve {estrato35.ToString("C")} (35%), para HOBBIES");
+            resultados.Items.Add($"- Reserve {estrato15.ToString("C")} (15%), para INVESTIMENTOS");
+        }
+
+        private void OrganizarRenda(object sender = null, EventArgs e = null)
+        {
+            try
+            {
+                double rendaCliente = double.Parse(valorRenda.Text);
+                EstratificarRenda(rendaCliente);
+            }
+            catch
+            {
+                MessageBox.Show("Valor Inválido\r\n" +
+                    "não insira palavras ou símbolos\r\n" +
+                    "Apenas números (Ex: 4500,50) ");
+            }
+        }
+    }
+}
